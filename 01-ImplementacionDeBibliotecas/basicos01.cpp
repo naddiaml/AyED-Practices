0b9cd4a6b084002f8a4c9f4c9b93bfef5302c3cc
naddiaml <naddiamlv@gmail.com>
2024-04-08 12:18:05 -0300

    :books: Básicos sobre uso de librerías

---
+++ 01-ImplementacionDeBibliotecas/basicos01.cpp
@@ -0,0 +1,13 @@
+// En caso de tener que agregar código adicional (implementación de Bibliotecas, librerías, etc), eso sucede aquí, y esas declaraciones son llamadas "Directivas pre-compilado".
+
+// La sintaxis para agregar una biblioteca es: #include <nombreDeLaBiblioteca>
+
+// Otras directivas pre-compilado
+
+int main()
+{
+
+    /* Instrucciones a ejecutar */
+
+    return 0;
+}
