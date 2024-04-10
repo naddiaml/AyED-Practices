04bba67d4ed743d805f3ef3a0434a85ec1d0ddf1
naddiaml <naddiamlv@gmail.com>
2024-04-09 22:58:03 -0300

    :heavy_plus_sign: Estructura básica del programa

---
+++ nocionesBasicas.cpp
@@ -0,0 +1,10 @@
+/* Declara la función principal o punto de entrada del programa. Debe llamarse main, de lo contrario el compilador no identificará ningún punto de entrada.
+Las llaves de la función ({ }), indican el comienzo y final del cuerpo de la función, es decir, lo que tiene que hacer la función cuando es ejecutada. */
+
+int main()
+{
+
+    /* Instrucciones a ejecutar */
+
+    return 0; /* Este valor de retorno, tiene que ver con un valor de retorno propio de la función main que indica que el programa se ejecutó correctamente.*/
+}
